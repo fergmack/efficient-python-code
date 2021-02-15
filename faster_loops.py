abb0f1083f4695b90c5cec76ed9e8bbd684a36a8
fergmack <33000542+fergmack@users.noreply.github.com>
2021-02-15 21:44:01 +0000

    Update faster_loops.py
    Review     # ----------- Holistic Conversion Loop -----------------

--- faster_loops.py
+++ faster_loops.py
@@ -28,3 +28,25 @@ for gen, count in gen_counts.items():
   print(
     'Generation {}: count = {:3} percentage = {}'.format(gen, count, gen_percent)
   )
+
+    # ----------- Holistic Conversion Loop -----------------
+from itertools import combinations 
+
+pokemon_types = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water']
+
+# # Collect all possible pairs using combinations()
+possible_pairs = [*combinations(pokemon_types, 2 )]
+
+# Gather all the possible pairs of Pokémon types.
+enumerated_tuples = []
+
+# # Append each enumerated_pair_tuple to the empty list above
+
+for i, pair in enumerate(possible_pairs, 1):
+    enumerated_pair_tuple = (i, ) + pair
+    enumerated_tuples.append(enumerated_pair_tuple)
+
+# # Convert all tuples in enumerated_tuples to a list
+enumerated_pairs = [*map(list, enumerated_tuples)]
+print(enumerated_pairs)
+
